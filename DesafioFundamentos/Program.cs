96ccd5691d87bcb2e0c6161e35b28eeff9e3341e
juliana sousa <sousa_julianacontato@outlook.com>
2024-12-28 02:06:32 -0300

    Projeto estacionamento do Desafio Fundamentos finalizado.

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,6 +1,5 @@
 ﻿using DesafioFundamentos.Models;
 
-// Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 decimal precoInicial = 0;
@@ -13,16 +12,17 @@ precoInicial = Convert.ToDecimal(Console.ReadLine());
 Console.WriteLine("Agora digite o preço por hora:");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
 
-// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
 
 string opcao = string.Empty;
 bool exibirMenu = true;
 
-// Realiza o loop do menu
 while (exibirMenu)
 {
-    Console.Clear();
+    if (Console.IsOutputRedirected == false)
+    {
+        Console.Clear();
+    }
     Console.WriteLine("Digite a sua opção:");
     Console.WriteLine("1 - Cadastrar veículo");
     Console.WriteLine("2 - Remover veículo");
